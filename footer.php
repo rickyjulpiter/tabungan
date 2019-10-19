e03df89ee6530e5bf829e75aa243cecce755aae8
geneticdie <wilyanto24@gmail.com>
2019-10-19 19:15:35 +0700

    update version

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	<footer class="main-footer">
       <div class="pull-right hidden-xs">
-        <b>Version</b> BETA
+        <b>Version</b> v1.0
       </div>
       <strong>Copyright &copy; 2019 
       	<!--<a href="https://sistempintar.com">Sistem Pintar</a>.-->
